92cf77dc8fa645063259febc3237ff1c81f1113c
James Darby <jamesdarby1@hotmail.com>
2020-03-31 17:05:46 +0100

    css tweeks

--- src/components/common/Home.js
+++ src/components/common/Home.js
@@ -3,7 +3,28 @@ import { Link } from 'react-router-dom'
 
 class Home extends React.Component {
   state = {
-    search: ''
+    search: '',
+    width: 0,
+    height: 0
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = { width: 0, height: 0 }
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this)
+  }
+  
+  componentDidMount() {
+    this.updateWindowDimensions()
+    window.addEventListener('resize', this.updateWindowDimensions)
+  }
+  
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDimensions)
+  }
+  
+  updateWindowDimensions() {
+    this.setState({ width: window.innerWidth, height: window.innerHeight })
   }
 
   handleChange = e => {
@@ -21,12 +42,13 @@ class Home extends React.Component {
   }
 
   render() {
+    const { width } = this.state
     return (
       <>
         <div className="hero-body">
           <form onSubmit={this.handleSubmit} className="search-bar">
             <div className="search">
-              <input type="text" className="search-text" placeholder="Choose an area" onChange={this.handleChange} />
+              <input type="text" className="search-text" placeholder={width > 674 ? 'Choose an area' : 'Area'} onChange={this.handleChange} />
               <button type="submit" className="search-button">
                 <img src="../../assets/search.png" />
               </button>
@@ -34,134 +56,120 @@ class Home extends React.Component {
           </form>
           <a href="#skills" className="arrow"><img src="../../assets/arrow.png" /></a>
         </div>
-        <section>
+        <section className='image-section'>
           <div className="wrapper" id="skills">
             <div className="columns is-mobile is-multiline">
               <div className="skill column is-one-quarter-desktop is-half-tablet is-full-mobile">
-                <Link onClick={this.handleClick} to="/chefs">
-                  <img src="../../assets/african.jpeg" alt="African" id="African" />
-                  <div className="border" id="African">
-                    <h2 id="African">African</h2>
+                <Link onClick={this.handleClick} to="/chefs" className='african'>
+                  <div alt="African" id="African" >
+                    <div className='layer' id="African" >
+                      <h2 id="African" >African</h2>
+                    </div>
                   </div>
                 </Link>
               </div>
               <div className="skill column is-one-quarter-desktop is-half-tablet is-full-mobile">
-                <Link onClick={this.handleClick} to="/chefs">
-                  <img src="../../assets/caribbean.jpeg" alt="Caribbean" id="Caribbean"/>
-                  <div className="border" id="Caribbean">
-                    <h2 id="Caribbean">Caribbean</h2>
+                <Link onClick={this.handleClick} to="/chefs" className='carribean'>
+                  <div className='layer' id="Caribbean" >
+                    <h2 id="Caribbean" >Caribbean</h2>
                   </div>
                 </Link>
               </div>
               <div className="skill column is-one-quarter-desktop is-half-tablet is-full-mobile">
-                <Link onClick={this.handleClick} to="/chefs">
-                  <img src="../../assets/chinese.jpeg" alt="Chinese" id="Chinese"/>
-                  <div className="border" id="Chinese">
-                    <h2 id="Chinese">Chinese</h2>
+                <Link onClick={this.handleClick} to="/chefs" className='chinese'>
+                  <div className='layer' id="Chinese" >
+                    <h2 id="Chinese" >Chinese</h2>
                   </div>
                 </Link>
               </div>
               <div className="skill column is-one-quarter-desktop is-half-tablet is-full-mobile">
-                <Link onClick={this.handleClick} to="/chefs">
-                  <img src="../../assets/french.jpeg" alt="French" id="French"/>
-                  <div className="border" id="French">
-                    <h2 id="French">French</h2>
+                <Link onClick={this.handleClick} to="/chefs" className='french'>
+                  <div className='layer' id="French" >
+                    <h2 id="French" >French</h2>
                   </div>
                 </Link>
               </div>
               <div className="skill column is-one-quarter-desktop is-half-tablet is-full-mobile">
-                <Link onClick={this.handleClick} to="/chefs">
-                  <img src="../../assets/greek.jpeg" alt="Greek" id="Greek"/>
-                  <div className="border" id="Greek">
-                    <h2 id="Greek">Greek</h2>
+                <Link onClick={this.handleClick} to="/chefs" className='greek'>
+                  <div className='layer' id="Greek" >
+                    <h2 id="Greek" >Greek</h2>
                   </div>
                 </Link>
               </div>
               <div className="skill column is-one-quarter-desktop is-half-tablet is-full-mobile">
-                <Link onClick={this.handleClick} to="/chefs">
-                  <img src="../../assets/indian.jpeg" alt="Indian" id="Indian"/>
-                  <div className="border" id="Indian">
-                    <h2 id="Indian">Indian</h2>
+                <Link onClick={this.handleClick} to="/chefs" className='indian'>
+                  <div className='layer' id="Indian" >
+                    <h2 id="Indian" >Indian</h2>
                   </div>
                 </Link>
               </div>
               <div className="skill column is-one-quarter-desktop is-half-tablet is-full-mobile">
-                <Link onClick={this.handleClick} to="/chefs">
-                  <img src="../../assets/italian.jpeg" alt="Italian" id="Italian"/>
-                  <div className="border" id="Italian">
-                    <h2 id="Italian">Italian</h2>
+                <Link onClick={this.handleClick} to="/chefs" className='italian'>
+                  <div className='layer' id="Italian" >
+                    <h2 id="Italian" >Italian</h2>
                   </div>
                 </Link>
               </div>
               <div className="skill column is-one-quarter-desktop is-half-tablet is-full-mobile">
-                <Link onClick={this.handleClick} to="/chefs">
-                  <img src="../../assets/japanese.jpeg" alt="Japanese" id="Japanese"/>
-                  <div className="border" id="Japanese">
-                    <h2 id="Japanese">Japanese</h2>
+                <Link onClick={this.handleClick} to="/chefs" className='japanese'>
+                  <div className='layer' id="Japanese" >
+                    <h2 id="Japanese" >Japanese</h2>
                   </div>
                 </Link>
               </div>
               <div className="skill column is-one-quarter-desktop is-half-tablet is-full-mobile">
-                <Link onClick={this.handleClick} to="/chefs">
-                  <img src="../../assets/korean.jpeg" alt="Korean" id="Korean"/>
-                  <div className="border" id="Korean">
-                    <h2 id="Korean">Korean</h2>
+                <Link onClick={this.handleClick} to="/chefs" className='korean'>
+                  <div className='layer' id="Korean" >
+                    <h2 id="Korean" >Korean</h2>
                   </div>
                 </Link>
               </div>
               <div className="skill column is-one-quarter-desktop is-half-tablet is-full-mobile">
-                <Link onClick={this.handleClick} to="/chefs">
-                  <img src="../../assets/mexican.jpg" alt="Mexican" id="Mexican"/>
-                  <div className="border" id="Mexican">
-                    <h2 id="Mexican">Mexican</h2>
+                <Link onClick={this.handleClick} to="/chefs" className='mexican'>
+                  <div className='layer' id="Mexican" >
+                    <h2 id="Mexican" >Mexican</h2>
                   </div>
                 </Link>
               </div>
               <div className="skill column is-one-quarter-desktop is-half-tablet is-full-mobile">
-                <Link onClick={this.handleClick} to="/chefs">
-                  <img src="../../assets/moroccan.jpeg" alt="Moroccan" id="Moroccan" />
-                  <div className="border" id="Moroccan">
-                    <h2 id="Moroccan">Moroccan</h2>
+                <Link onClick={this.handleClick} to="/chefs" className='moroccan'>
+                  <div className='layer' id="Moroccan" >
+                    <h2 id="Moroccan" >Moroccan</h2>
                   </div>
                 </Link>
               </div>
               <div className="skill column is-one-quarter-desktop is-half-tablet is-full-mobile">
-                <Link onClick={this.handleClick} to="/chefs">
-                  <img src="../../assets/asian.jpeg" alt="South-East Asian" id="South-East Asian"/>
-                  <div className="border" id="South-East Asian">
-                    <h2 id="South-East Asian">South-East Asian</h2>
+                <Link onClick={this.handleClick} to="/chefs" className='asian'>
+                  <div className='layer' id="South-East Asian" >
+                    <h2 id="South-East Asian" >South-East Asian</h2>
                   </div>
                 </Link>
               </div>
               <div className="skill column is-one-quarter-desktop is-half-tablet is-full-mobile">
-                <Link onClick={this.handleClick} to="/chefs">
-                  <img src="../../assets/spanish.jpg" alt="Spanish" id="Spanish"/>
-                  <div className="border" id="Spanish">
-                    <h2 id="Spanish">Spanish</h2>
+                <Link onClick={this.handleClick} to="/chefs" className='spanish'>
+                  <div className='layer' id="Spanish" >
+                    <h2 id="Spanish" >Spanish</h2>
                   </div>
                 </Link>
               </div>
               <div className="skill column is-one-quarter-desktop is-half-tablet is-full-mobile">
-                <Link onClick={this.handleClick} to="/chefs">
-                  <img src="../../assets/turkish.jpg" alt="Turkish/Middle-Eastern" id="Turkish/Middle-Eastern"/>
-                  <div className="border" id="Turkish/Middle-Eastern">
-                    <h2 id="Turkish/Middle-Eastern">Turkish/Middle-Eastern</h2>
-                  </div>
+                <Link onClick={this.handleClick} to="/chefs" className='turkish'>
+                  <div className='layer' id="Turkish/Middle-Eastern" >
+                    <h2 id="Turkish/Middle-Eastern" >Turkish <br /> Middle-Eastern</h2>
+                  </div>  
                 </Link>
               </div>
               <div className="skill column is-one-quarter-desktop is-half-tablet is-full-mobile">
-                <Link onClick={this.handleClick} to="/chefs">
-                  <img src="../../assets/vegan.jpeg" alt="Vegan" id="Vegan" />
-                  <div className="border" id="Vegan">
-                    <h2 id="Vegan">Vegan</h2>
-                  </div>
+                <Link onClick={this.handleClick} to="/chefs" className='vegan'>
+                  <div className='layer' id="Vegan" >
+                    <h2 id="Vegan" >Vegan</h2>
+                  </div>  
                 </Link>
               </div>
               <div className="skill column is-one-quarter-desktop is-half-tablet is-full-mobile">
-                <Link onClick={this.handleClick} to="/chefs">
-                  <img src="../../assets/vegetarian.jpg" alt="Vegetarian" id="Vegetarian"/>
-                  <div className="border" id="Vegetarian">
-                    <h2 id="Vegetarian">Vegetarian</h2>
+                <Link onClick={this.handleClick} to="/chefs" className='vegetarian'>
+                  <div className='layer' id="Vegetarian" >
+                    <h2 id="Vegetarian" >Vegetarian</h2>
                   </div>
                 </Link>
               </div>
--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -6,7 +6,8 @@ import Auth from '../../lib/auth'
 class Navbar extends React.Component {
   state = {
     navbarOpen: false,
-    searchResult: null
+    searchResult: null,
+    isActive: false
   }
 
   toggleNavBar = () => {
@@ -15,6 +16,11 @@ class Navbar extends React.Component {
 
   handleClick = (e) => {
     localStorage.setItem('skill', e.target.innerHTML)
+  }
+
+  activeClick = () => {
+    console.log('working')
+    this.setState({ isActive: !this.state.isActive })
   }
 
   handleLogout = () => {
@@ -29,7 +35,8 @@ class Navbar extends React.Component {
   }
 
   render() {
-    const { navbarOpen } = this.state
+    const { navbarOpen, isActive } = this.state
+    const menuVis = isActive ? 'is-active' : ''
     return (
       <nav className="navbar nav is-transparent is-fixed-top">
         <div className="container">
@@ -38,12 +45,18 @@ class Navbar extends React.Component {
             <Link className="navbar-item" to="/">
               <h1>Chow Vow</h1>
             </Link>
+            <a role="button" onClick={this.activeClick} className={`navbar-burger burger ${menuVis}`} aria-label="menu" aria-expanded="false">
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
           </div>
-          <Link className="navbar-item has-text-white" to="/map/london">MAP</Link>
-          <div className="navbar-item has-dropdown is-hoverable">
-            <a className="navbar-link has-text-white is-arrowless" onClick={this.toggleNavBar}>CHEFS</a>
-            {
-              navbarOpen &&
+          <div className={`navbar-menu ${menuVis}`}>
+            <Link className="navbar-item has-text-white" to="/map/london">MAP</Link>
+            <div className="navbar-item has-dropdown is-hoverable">
+              <a className="navbar-link has-text-white is-arrowless" onClick={this.toggleNavBar}>CHEFS</a>
+              {
+                navbarOpen &&
               <div className="navbar-dropdown">
                 <Link className="navbar-item" to="/chefs" onClick={this.handleClick}>ALL</Link>
                 <Link className="navbar-item" to="/chefs" onClick={this.handleClick}>African</Link>
@@ -63,14 +76,15 @@ class Navbar extends React.Component {
                 <Link className="navbar-item" to="/chefs" onClick={this.handleClick}>Vegan</Link>
                 <Link className="navbar-item" to="/chefs" onClick={this.handleClick}>Vegetarian</Link>
               </div>
-            }
-          </div>
-          <div className="navbar-end">
-            {!Auth.isAuthenticated() && <Link className="navbar-item has-text-white" to="/register">SIGN UP</Link>}
-            {!Auth.isAuthenticated() && <Link className="navbar-item has-text-white" to="/login">LOGIN</Link>}
-            {Auth.isAuthenticated() && <Link className="navbar-item has-text-white" to="/offers">OFFERS</Link>}
-            {Auth.isAuthenticated() && <Link className="navbar-item has-text-white" to="/profile">PROFILE</Link>}
-            {Auth.isAuthenticated() && <a className="navbar-item has-text-white" onClick={this.handleLogout}>LOGOUT</a>}
+              }
+            </div>
+            <div className="navbar-end">
+              {!Auth.isAuthenticated() && <Link className="navbar-item has-text-white" to="/register">SIGN UP</Link>}
+              {!Auth.isAuthenticated() && <Link className="navbar-item has-text-white" to="/login">LOGIN</Link>}
+              {Auth.isAuthenticated() && <Link className="navbar-item has-text-white" to="/offers">OFFERS</Link>}
+              {Auth.isAuthenticated() && <Link className="navbar-item has-text-white" to="/profile">PROFILE</Link>}
+              {Auth.isAuthenticated() && <a className="navbar-item has-text-white" onClick={this.handleLogout}>LOGOUT</a>}
+            </div>
           </div>
         </div>
       </nav>
