ac36b6c645bc6817ad1dce0c9b810072b811dd86
James Darby <jamesdarby1@hotmail.com>
2020-10-30 13:46:04 +0000

    removed edit profile to reduce friction

--- src/components/brands/BrandProfile.js
+++ src/components/brands/BrandProfile.js
@@ -2,30 +2,62 @@ import propTypes from 'prop-types'
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
-import { getToken } from '../lib/auth'
+import { getToken, getUser } from '../lib/auth'
 import Navbar from '../common/Navbar'
-import folder from '../../styles/images/folder.svg'
+import BrandFiles from '../brands/BrandFiles'
+import BrandImages from './BrandImages'
+import BrandLogo from './BrandLogo'
+import BrandCover from './BrandCover'
 
+/** Component for displaying and editing profile information */
 const Profile = ({
   match: {
     params: { id }
   }
 }) => {
   const [user, setData] = useState({})
+  const [success, setSuccess] = useState(false)
 
   useEffect(() => {
-    const getData = async () => {
-      try {
-        const res = await axios.get(`/api/brands/${id}`, {
-          headers: { Authorization: `Bearer ${getToken()}` }
-        })
-        setData(res.data)
-      } catch (err) {
-        console.log(err)
-      }
-    }
     getData()
   }, [id])
+
+  const getData = async () => {
+    try {
+      const res = await axios.get(`/api/brands/${id}`, {
+        headers: { Authorization: `Bearer ${getToken()}` }
+      })
+      setData(res.data)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  /** Submit new profile information. */
+  const handleSubmit = async e => {
+    e.preventDefault()
+    try {
+      // currently inefficient because it sends up data that might not be changed
+      const newData = {
+        username: user.username,
+        email: user.email,
+        summary: user.summary,
+        blog: user.blog,
+        website: user.website,
+        cover: user.cover
+      }
+      await axios
+        .put(`/api/brands/${getUser()}`, newData, {
+          headers: { Authorization: `Bearer ${getToken()}` }
+        })
+      setSuccess(!success)
+      setTimeout(() => {
+        setSuccess(false)
+      }, 3000)
+    } catch (err) {
+      console.log(err)
+    }
+  }
 
   return (
     <main>
@@ -33,89 +65,89 @@ const Profile = ({
       <section className="brandprofile">
         <div className="left-side">
           <div className="branding">
-            {user.cover ? (
-              <img className="user-cover" src={user.cover} alt={user.cover} />
-            ) : (
-              
-              <Link to={`/profile/${id}/edit`} className="user-cover">
-                <div>                  
-                  <img className="cover" src="" alt="no cover" />
-                </div>
-              </Link>
-            )}
-            {user.logo ? (
-              <img className="profile-logo" src={user.logo} alt="Logo" />
-            ) : (
-              <Link to={`/profile/${id}/edit`} className="logo-circle">
-                <img className="logo" src="" alt="no logo" />
-              </Link>
-            )}</div>
+            <BrandCover user={user} getData={getData}/>
+            <BrandLogo user={user} getData={getData}/>
+          </div>
           <hr/>
           <div className="sect">
-            <h2 style={{ display: 'inline' }}>Name:</h2> <p style={{ display: 'inline' }}>{user.username}</p> <br/>
-            <h2 style={{ display: 'inline' }}>Website:</h2> <p style={{ display: 'inline' }}>{user.website ? user.website : ''}</p> <br/>
-            <h2 style={{ display: 'inline' }}>Blog/refernce url:</h2> <p style={{ display: 'inline' }}>{user.blog ? user.blog : ''}</p>
-          </div>
-          <hr />
-          <div className="sect">
-            <h2>Summary:</h2> <p>{user.summary ? user.summary : ''}</p>
-          </div>
-          <hr />
-          <div className="sect">
-            {user.image && user.image.length ? (
-              user.image &&
-            user.image.map((img, i) => (
-              <img
-                className="brand-imagery"
-                src={img.url}
-                key={i}
-                alt={`${user.username} image`}
+            <form onSubmit={(e) => handleSubmit(e)}>
+              <h2 style={{ display: 'inline' }}>Name:</h2>
+              <input 
+                style={{ display: 'inline' }} 
+                value={user.username || ''} 
+                placeholder="Brandname"
+                name="username"
+                onChange={({ target: { name, value } }) =>
+                  setData({ ...user, [name]: value })
+                }/> 
+              <br/>
+              <h2 style={{ display: 'inline' }}>Email:</h2> 
+              <input 
+                style={{ display: 'inline' }} 
+                value={user.email || ''} 
+                placeholder="Email"
+                name="email"
+                onChange={({ target: { name, value } }) =>
+                  setData({ ...user, [name]: value })
+                }/> 
+              <br/>
+              <h2 style={{ display: 'inline' }}>Website:</h2> 
+              <input 
+                style={{ display: 'inline' }} 
+                value={user.website || ''} 
+                placeholder="Website"
+                name="website"
+                onChange={({ target: { name, value } }) =>
+                  setData({ ...user, [name]: value })
+                }/> 
+              <br/>
+              <h2 style={{ display: 'inline' }}>Blog/refernce url:</h2> 
+              <input 
+                style={{ display: 'inline', width: '25%' }} 
+                value={user.blog || ''}
+                placeholder="Blog/refernce url"
+                name="blog"
+                onChange={({ target: { name, value } }) =>
+                  setData({ ...user, [name]: value })
+                } 
               />
-            ))
-            ) : (
-              <Link to={`/profile/${id}/edit`} className="no-image">
-                <div>Add images</div>
-              </Link>
-            )}
+              <br/>
+              <button type='submit' style={{ marginRight: '10px' }} className="button is-small blue is-rounded">Save</button>
+              {success ? 'Success!' : null}
+            </form>
           </div>
           <hr />
           <div className="sect">
-            <div className="documents">
-              {user.docs && user.docs.length ? (
-                user.docs.map((img, i) => (
-                  <div className="docs" key={i}>
-                    <a href={img.url} target='_blank' rel='noopener noreferrer'>
-                      <img
-                        className="file-imagery"
-                        src={folder}
-                        key={i}
-                        alt={`${user.username} docs`}
-                      />
-                    </a>
-                    {img.name}
-                  </div>
-                ))
-              ) : (
-                <Link to={`/profile/${id}/edit`} className="no-image">
-                  <div>Add docs</div>
-                </Link>
-              )}
-            </div>
+            <h2>Summary:</h2>
+            <form onSubmit={(e) => handleSubmit(e)}>
+              <textarea 
+                value={user.summary || ''} 
+                style={{ width: '100%' }} 
+                rows="16"
+                name="summary"
+                onChange={({ target: { name, value } }) =>
+                  setData({ ...user, [name]: value })
+                }
+              />
+              <button type='submit' style={{ marginRight: '10px' }} className="button is-small blue is-rounded">Save</button>
+              {success ? 'Success!' : null}
+            </form> 
+          </div>
+          <hr />
+          <div className="sect">
+            <BrandImages user={user} getData={getData}/>
+          </div>
+          <hr />
+          <div className="sect">
+            <BrandFiles user={user} getData={getData}/>
           </div>
           <hr />
           <div className="sect">
             <p>
-              The Writers assigned to your briefs will see these references:
+              The Writers assigned to your briefs will see this information
             </p>
           </div>
           <hr/>
-          <div className="sect">
-            <Link to={`/profile/${id}/edit`}>
-              <button type="button" className="button is-rounded is-large">
-              Edit profile
-              </button>
-            </Link>
-          </div>
         </div>
         <div className="right-side">
           <h3>Report Summary</h3>
