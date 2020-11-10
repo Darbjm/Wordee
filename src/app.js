db0558f0c02067b95e0f9d16bfac6306ae594806
James Darby <jamesdarby1@hotmail.com>
2020-11-10 00:39:31 +0000

    able to add and edit user in state

--- src/app.js
+++ src/app.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom'
 import './styles/main.scss'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import 'bulma'
+import { Provider } from 'react-redux'
 
+import store from './redux/store'
 import SecureRoute from './components/common/SecureRoute'
 import Login from './components/auth/Login'
 import Register from './components/auth/Register'
@@ -16,21 +18,21 @@ import BackEndLogin from './components/auth/BackEndLogin'
 
 const App = () => (
   <BrowserRouter>
-  <>
-    <Switch>
-      <Route exact path="/" component={Login} />
-      <Route path="/register" component={Register} />
-      <Route path="/admin" component={BackEndLogin} />
-      <SecureRoute path="/backendtesting5702918301" component={BackEnd} />
-      <SecureRoute path="/profile/:id" component={Profile} />
-      <SecureRoute path="/createbrandbrief" component={Brandbrief} />
-      <SecureRoute path="/editbrief/:id" component={BrandEditBrief} />
-      <Route path="/*" component={FailedPage} />
-    </Switch>
-  </>
+      <>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route path="/register" component={Register} />
+        <Route path="/admin" component={BackEndLogin} />
+        <SecureRoute path="/backendtesting5702918301" component={BackEnd} />
+        <SecureRoute path="/profile/:id" component={Profile} />
+        <SecureRoute path="/createbrandbrief" component={Brandbrief} />
+        <SecureRoute path="/editbrief/:id" component={BrandEditBrief} />
+        <Route path="/*" component={FailedPage} />
+      </Switch>
+      </>
   </BrowserRouter>
 )
 ReactDOM.render(
-  <App />,
+  <Provider store={store}><App /></Provider>,
   document.getElementById('root')
 )
